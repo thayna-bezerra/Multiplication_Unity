c948799bfae3478846ad0e9b1157b1f062c0b6f3
thayna-bezerra <arraisthayna2106@gmail.com>
2022-09-18 18:41:39 -0300

    Conferindo resposta com clique

---
+++ Multiplication/Assets/Scripts/CheckAnswer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckAnswer : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        if(this.gameObject.tag == "Resposta")
+        {
+            Debug.Log("Essa é a resposta");
+        }
+
+        if(this.gameObject.tag == "Errado")
+        {
+            Debug.Log("Resposta ERRADA");
+        }
+    }
+}
